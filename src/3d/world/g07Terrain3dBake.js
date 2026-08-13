33384406a31f977d86dbe9050bd1fd4c9256c4d4
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-08-13 07:06:32 +0300

    terrain(sw): add compact G07 Terrain3D runtime payload

---
+++ src/3d/world/g07Terrain3dBake.js
@@ -0,0 +1,213 @@
+/**
+ * Günbatımı Ustası / G07 compact Three.js runtime payload.
+ *
+ * Generated only from the real pinned Terrain3D v1.0.2 bake artifact produced by CI run
+ * 31665397412. The 65x65 Terrain3D-aligned proof is sampled at every second node (33x33) and
+ * quantized for a small synchronous browser payload. CI re-bakes Terrain3D and byte-compares the
+ * packed values before this module can be considered parity-safe.
+ */
+export const G07_TERRAIN3D_RUNTIME_BAKE = Object.freeze({
+	id: 'gunbatimi-ustasi-g07-terrain3d-runtime-bake-2026-08-13-v2',
+	artifactRunId: 31665397412,
+	artifactId: 9167770008,
+	artifactDigestSha256: 'a1d33e7b98cccba28241300a95b18a18da0844a4a9d7620c2f92af7bdd73a3f3',
+	canonicalBakeSha256: 'b534ab9c162872d23e6d46577c752f3f44d125fea87be3c83e1b0caa52bd4acd',
+	importedTopdownSha256: '1f54dc059b602736f00f1078795c5b75dd9642cb0888af48ed5afe88b571f38f',
+	sourceMapSha256: '20702972e8f45f0fbdc4da5fa68e890a82e4e822e1d58e2f369d8bc5b9c571a1',
+	terrain3dVersion: '1.0.2',
+	terrain3dImportSize: 257,
+	terrain3dRegionSize: 256,
+	terrain3dRegionCount: 4,
+	terrain3dBakedVertices: 263169,
+	terrain3dSavedRegionFiles: 4,
+	gridSize: 33,
+	normalizedBounds: Object.freeze({ xMin: 0, xMax: 0.125, yMin: 0.875, yMax: 1 }),
+	heightOffsetMeters: -9,
+	heightQuantumMeters: 0.0001,
+	maxDownsampleHeightErrorMeters: 0.00188423,
+	maxDownsampleRockError: 0.0019608,
+	maxDownsampleColorError: 0.0019608,
+	maxDownsampleRoughnessError: 0.00294118,
+	maxTerrain3dRoundtripHeightErrorMeters: 0.00000048,
+	maxTerrain3dFractionalHeightErrorMeters: 0.00000110,
+	maxTerrain3dBoundaryHeightStepMeters: 0.00296498,
+});
+
+export const G07_TERRAIN3D_PACKED_HEIGHTS_U16_LE_BASE64 =
+	'diqNKgor2ivkLBEuSS94MIkxbTITM24zdjMiM28yXDHrLyMuCiyuKR0nZySfIdweMxy9GZQX0xWVFI8TkRKZEaoQoSudK+crhyxkLWYudy+CMHQxPDLLMhUz' +
+	'DzOzMv0x7DCDL8ctwCt6KQMnbCTHISofqxxjGmoY2xbAFbsUvBPEEtUR1izLLOIsUS0ALtguwS+pMHsxKDKgMtcyxDJgMqcxmDA2L4ctkSthKQUnjSQMIpUf' +
+	'QB0lG1wZABj1FvAV8RT6EwoTFS4JLvotOC65LmUvKDDrMJ4xLzKQMrUylTIpMm0xYDAFL2ItfitlKSMnyiRsIh0g8h0EHGwaPhk0GC8XMBY4FUkUXC9QLy4v' +
+	'Oy+OLw8wqjBKMdwxUjKcMq8ygTIMMk0xQzDwLlkthyuDKV0nIyXoIsAgwB4AHZgbhRp6GXUYdhd/Fo8VqDCdMHowWjB+MNUwRzHDMTYykDLDMsMyiDIMMkkx' +
+	'QTD2Lmstqiu9KbInlyWAI34hqR8XHuAc0RvHGsIZwxjMF9wW+jHvMcwxljGLMbUxADJYMqsy6TIFM/MyqzImMmExWzAWL5gt6SsSKiIoJyYyJFkiriBLHzIe' +
+	'Ix0ZHBQbFRoeGS4YUDNEMyIz6TKyMrEy1DIHMzozXTNhMz0z6DJaMpIxjzBSL+AtQiyCKq0o0SYAJU4jzyGZIIgfeR5vHWocaxtzGoQZqTSdNHo0QTT1M8gz' +
+	'wzPSM+Qz6zPYM6IzPzOqMt4x3TCnL0IutiwMK1IplifpJV4kCiPyIeAg0h/HHsIdwxzMG9waAjb2NdQ1mzVNNfo0zDS2NKk0kzRqNCE0sTMTM0UyRjEXML8u' +
+	'RC2xKxIqdSjsJoklXyRLIzoiKyEhIBwfHR4lHTYcXDdQNy039DanNkU27zW1NYc1VjUVNbo0PTSXM8UyyDGhMFYv7C1vLOsqbikJKM4muCWlJJMjhSJ7IXYg' +
+	'dx9/HpAdtDioOIY4TTj/N543LDfONn82MjbaNW014jQ0NGAzZTJFMQYwri5ILd8rgSpAKSUoESf9Jewk3SPTIs4hzyDXH+geCjr+Ods5ojlVOfM4fzgAOJE3' +
+	'KDe5Njk2oTXrNBM0GzMDMtAwii86LuwsriuPKnspZihTJ0EmMyUpJCQjJSItIT4gWztQOy079DqmOkU60TlLOb04ODixNx83eTa7NeE06jPZMrMxfzBFLxMu' +
+	'9CzhK80quCmkKJMnhSZ6JXUkdiN/Io8hqDycPHk8QDzzO5E7HTuXOgA6YDnDOB44azekNsc10jTJM7AyjTFqMFIvQS4tLRksBCvxKd8o0SfHJsIlwyTLI9wi' +
+	'7j3iPb89hj05Pdc8YzzdO0Y7oDrtOTY5dTimN8Y21DXSNMUzszKmMZgwhy9zLl8tSiw3KyUqFykMKAcnCCYRJSEkLD8gP/0+xD53PhU+oT0bPYQ83jsoO2Y6' +
+	'mDnBON437jbzNfM07jPkMtYxxTCxL50uiC11LGMrVSpKKUUoRidPJl8lYEBVQDJA+T+rP0o/1j5QPrk9Ej1dPJs7yzrxOQs5HTgmNyc2IzUZNAsz+THmMNEv' +
+	'vS6pLZgsiSt/KnopeyiEJ5Qmi0F/QVxBI0HWQHRAAEB6P+M+PT6IPcU89jsbOzY6RzlQOFI3TTZDNTU0JDMQMvww5y/ULsIttCypK6QqpSmuKL4nqUKdQntC' +
+	'QkL0QZNBHkGYQAJAWz+mPuM9FD05PFQ7ZjpuOXA4azdiNlM1QjQvMxoyBjHyL+Eu0i3ILMMrxCrMKd0ou0OvQ4xDU0MGQ6RCMEKqQRNBbUC3P/U+Jj5LPWY8' +
+	'dzuAOoI5fThzN2U1QTM/MjoxOzBDL1Qux0i8SJlIYEgSSLFHPUe3RiBGeUXERAJEMkNYQnJBhECNP44+ij2APHI7YDpNOTk4JDcQNv808TPmMuEx4jDrL/su' +
+	'WUlNSSpJ8UikSEJIzkdIR7FGC0ZWRZNExEPpQgRCFUEeQCA/Gz4RPQM88jreOco4tTeiNpA1gjR4M3MydDF8MI0v00nHSaRJa0keSbxISEjCRytHhUbQRQ1F' +
+	'PkRjQ35Cj0GYQJo/lT6LPX08bDtYOkQ5LzgcNwo2/DTyM+wy7jH2MAcwNEooSgVKzEl/SR1JqUgjSIxH5kYxRm5Fn0TEQ99C8EH5QPs/9j7sPd48zTu5OqU5' +
+	'kDh9N2s2XTVTNE0zTzJXMWgwe0pvSkxKE0rGSWRJ8EhqSNNHLUd4RrVF5kQLRCZDN0JAQUJAPT8zPiU9FDwAO+w51zjEN7I2pDWZNJQzlTKeMa8wpkqaSnhK' +
+	'P0rxSZBJG0mVSP9HWEejRuBFEUU2RFFDY0JrQW1AaD9fPlA9PzwsOxc6AznvN942zzXFNMAzwTLJMdowtUqpSoZKTkoASp5JKkmkSA1IZ0eyRu9FIEVFRGBD' +
+	'cUJ6QXxAdz9tPl89Tjw7OyY6Ejn+N+w23jXUNM8z0DLYMekw';
+
+export const G07_TERRAIN3D_PACKED_ROCK_U8_BASE64 =
+	'VFRUVVZWV1hZWlpaWlpZWFdWVFJQTkxKSEVDQkA/Pjw7VFRVVVZXWFhZWlpaWlpZWFdWVFJQTkxKSEZEQkFAPj08VVVVVlZXWFlZWlpaWlpZWFdWVFJQTkxK' +
+	'SEZFQ0JBPz49VlZWVldXWFlZWlpaWlpZWFdWVFJRT01LSUdFRENBQD89V1dXV1dYWVlaWlpaWlpZWFdWVFJRT01LSUhGRURCQUA+WFhYWFhYWVlaWlpaWlpZ' +
+	'WFdWVFNRT01MSkhHRkRDQkA/WFhYWFlZWVpaWlpaWlpZWFdWVFNRUE5MS0lIRkVEQkFAWVlZWVlaWlpaW1taWlpZWFdWVVNSUE5NS0pJR0ZFQ0JBWlpaWlpa' +
+	'WltbW1tbWlpZWFdWVVNSUE9NTEtJSEdFRENBW1tbW1tbW1tbW1tbW1pZWVhWVVRSUU9OTUtKSUhGRURCXFxcXFxcXFxcXFxbW1paWVhXVVRTUVBPTkxLSkhH' +
+	'RkRDXFxcXFxcXFxcXFxcW1taWVhXVlVTUlFQTk1MSklIRkVEXV1dXV1dXV1dXVxcXFtaWllYVlVUU1FQT05MS0pJR0ZEXl5eXl5eXl5eXV1dXFtbWllYV1ZV' +
+	'U1JRUE9NTEtJSEdFX19fX19fX15eXl5dXVxbWlpZV1ZVVFNSUE9OTUtKSUdGX19fX19fX19fX15eXVxcW1pZWFdWVVRSUVBPTUxLSUhHYGBgYGBgYGBgX19e' +
+	'Xl1cXFtaWVhXVVRTUlFPTk1LSklHYWFhYWFhYWBgYF9fXl5dXFtaWVhXVlVUU1FQT01MS0lIYWFhYWFhYWFhYWBgX15eXVxbWllYV1ZUU1JRT05NS0pJYmJi' +
+	'YmJiYmJhYWFgYF9eXV1cW1pYV1ZVVFJRUE9NTEtJYmJiYmJiYmJiYmFhYF9fXl1cW1pZWFdWVFNSUE9OTEtKY2NjY2NjY2NjYmJhYWBfXl5dXFtaWFdWVVRS' +
+	'UVBOTUxKY2NjY2NjY2NjY2JiYWFgX15dXFtaWVhXVVRTUVBPTUxLZGRkZGRkZGRjY2NiYmFgX19eXVxbWVhXVlVTUlFPTk1LZGRkZGRkZGRkZGNjYmFhYF9e' +
+	'XVxbWllXVlVUUlFQTk1MZWVlZWVlZWVkZGRjYmJhYF9eXVxbWllYV1VUU1FQT01MZWVlZWVlZWVlZGRjY2JhYWBfXl1cW1lYV1ZUU1JQT05MZWVlZWVlZWVl' +
+	'ZWRkY2JiYWBfXl1cW1pYV1ZVU1JRT05NZmZmZmZmZmVlZWRkY2NiYWBfXl1cW1pZV1ZVVFJRUE5NZmZmZmZmZmZlZWVkY2NiYWBfXl1cW1pZWFZVVFJRUE5N' +
+	'ZmZmZmZmZmZlZWVkZGNiYWFgX15dW1pZWFdVVFNRUE9NZmZmZmZmZmZmZWVkZGNiYmFgX15dW1pZWFdVVFNRUE9NZmZmZmZmZmZmZWVkZGNiYmFgX15dW1pZ' +
+	'WFdVVFNRUE9N';
+
+export const G07_TERRAIN3D_PACKED_COLOR_ROUGHNESS_U8_BASE64 =
+	'GzdCyBs2QsgbNkLIGzZCyRs2QskbN0LJGzdDyBs3Q8gbN0PIGzdDyBs3Q8cbN0PHGzdDxxs3Q8cbN0PHGzdDxxs3Q8gaNkLIGjZCyBo1QsgaNUHIGTRByBk0' +
+	'QMgZM0DIGDM/xxgyP8cYMj7HGDI+xxgxPsYXMT3GFzE9xhcxPcUXMT3FGzdDyBs3QsgbN0LIGzdCyBs3QsgbN0PIGzdDyBs3Q8gbN0PHGzdDxxs3Q8cbN0PH' +
+	'GzdDxxs3Q8cbN0PHGzdDxxs3Q8caNkLIGjZCyBo1QcgaNUHIGTRByBk0QMgZM0DIGTM/xxgzP8cYMj7HGDI+xxgyPscYMT7GFzE9xhcxPcYXMT3FGzdDyBs3' +
+	'Q8gbN0PIGzdDyBs3Q8gbN0PIGzdDyBs3Q8cbN0PHGzdDxxs3Q8YbN0PGGzdDxhs3Q8YbN0PHGzdDxxs3Q8caNkLIGjZCyBo1QcgaNUHIGTVByBk0QMgZNEDI' +
+	'GTM/xxgzP8cYMj7HGDI+xxgyPscYMj7GGDI+xhgxPsYXMT3FGzdDyBs3Q8kbN0PJGzdDyBs3Q8gbN0PIGzdDxxs3Q8cbN0PHGzdDxhs3Q8YbN0PGGzdDxhs3' +
+	'Q8YbN0PGGzdDxxs2QscaNkLIGjZCyBo1QcgaNUHIGTVByBk0QMgZNEDIGTM/xxkzP8cYMz/HGDI/xxgyPsYYMj7GGDI+xhgyPsYYMj7FHDhDyRs4Q8kbOEPJ' +
+	'GzdDyRs3Q8gbN0PIGzdDxxs3Q8cbN0PGGzdDxhs3Q8YbN0PGGzdDxhs3Q8YbN0PGGzdDxxs2QscaNkLHGjZCyBo2QcgaNUHIGjVByBk0QMgZNEDHGTRByRkz' +
+	'P8cYMz/HGDM/xxgzP8YYMj7GGDI+xhgyPsYYMj7FHDhEyRw4Q8kbOEPJGzdDyRs3Q8gbN0PIGzdDxxs3Q8cbN0PGGzdDxhs3Q8YbN0PGGzdDxhs3Q8YbN0PG' +
+	'GzdDxxs3QscaNkLHGjZCyBo2QsgaNUHIGjVByBk0QMgZNEDHGTRAxxkzP8cZMz/HGTM/xhgzP8YYMz/GGDI/xhgyPsUYMj7FHDhEyhw4RMkcOETJHDhEyRw4' +
+	'RMgcOEPIGzhDxxs4Q8cbOEPGGzhDxhs3Q8YbN0PGGzdDxhs3Q8YbN0PHGzdDxxs3QscbNkLIGjZCyBo2QsgaNUHIGjVByBo1QccZNEDHGTRAxxk0QMcZMz/G' +
+	'GTM/xhkzP8YYMz/GGDM/xRgzP8UYMj7FHDlEyhw4RMocOETJHDhEyRw4RMkcOETIHDhExxw4Q8cbOEPGGzhDxhs4Q8YbOEPGGzdDxhs3Q8cbN0PHGzdDxxs3' +
+	'Q8gbNkLIGjZCyBo2QsgaNUHIGjVBxxo1QccZNEDHGTRAxxk0QMYZNEDGGTRAxhkzP8YZMz/FGTM/xRgzP8UYMz/FHDlEyhw5RMocOETKHDhEyRw4RMkcOETI' +
+	'HDhExxw4RMccOETHHDhExhs4Q8YbOEPHGzhDxxs3Q8cbN0PIGzdDyBs3Q8gbN0LIGzZCyBo2QsgaNkHIGjVBxxo1QccaNUHHGTRAxhk0QMYZNEDGGTRAxRk0' +
+	'QMUZMz/FGTM/xRkzP8UZMz/FHDlFyhw5RcocOUTKHDlEyRw5RMkcOETIHDhEyBw4RMccOETHHDhExxw4RMccOETHGzhDxxs3Q8gbN0PIGzdDyBs3Q8kbN0PJ' +
+	'GzZCyBs2QsgaNkLIGjZBxxo1QccaNUHGGjVBxhk0QMYZNEDFGTRAxRk0QMUZNEDFGTRAxRkzP8UZMz/EHTlFyhw5RcocOUXKHDlFyRw5RckcOUTIHDlEyBw4' +
+	'RMccOETHHDhExxw4RMccOETIHDhEyBs4Q8gbN0PJGzdDyRs3Q8kbN0PJGzdCyRs2QsgaNkLIGjZCxxo2QccaNUHGGjVBxho1QcYaNUHFGTRAxRk0QMUZNEDF' +
+	'GTRAxBk0QMQZMz/EHTpFyh05RcodOUXKHDlFyRw5RckcOUXIHDlEyBw5RMgcOUTIHDhEyBw4RMgcOETIHDhEyRw4RMkbOEPJGzdDyRs3Q8obN0PJGzdDyRs3' +
+	'QskbNkLIGzZCyBo2QscaNkLGGjZBxho1QcUaNUHFGjVBxRo1QcUaNUHEGTRAxBk0QMQZNEDEHTpGyh06RsodOkXJHTpFyR06RckdOUXIHDlFyBw5RcgcOUTI' +
+	'HDlEyBw4RMgcOETJHDhEyRw4RMkcOETKGzhDyhs3Q8obN0PKGzdDyhs3Q8kbN0LIGzZCyBs2QscaNkLGGjZCxho2QcUaNUHFGjVBxRo1QcUaNUHFGjVBxBo1' +
+	'QMQZNEDEHTtGyh06RskdOkbJHTpGyR06RcgdOkXIHTlFyBw5RcgcOUXIHDlEyBw5RMkcOUTJHDhEyhw4RMocOETKHDhEyhs4Q8obN0PKGzdDyhs3Q8kbN0PJ' +
+	'GzdCyBs2QscbNkLHGzZCxho2QsYaNkLFGjZBxRo1QcUaNUHFGjVBxRo1QcUaNUHFHTtGyR07RskdOkbJHTpGyB06RsgdOkXIHTpFyB06RcgdOUXIHDlFyRw5' +
+	'RckcOUTKHDlEyhw4RMocOETLHDhEyxw4RMscOEPKGzdDyhs3Q8kbN0PJGzdDyBs3Q8cbN0LHGzZCxhs2QsYaNkLFGjZCxRo2QcUaNUHFGjVBxRo1QcUaNUHF' +
+	'HjtHyR47RskdO0bIHTtGyB07RsgdOkbIHTpGyB06RsgdOkXIHTpFyR06RckdOUXKHDlFyxw5RcscOUTLHDlEyxw4RMscOETLHDhEyhw4RMocOEPJGzdDyBs3' +
+	'Q8gbN0PHGzdDxxs3Q8YbN0LGGzZCxhs2QsYaNkLGGjZCxho2QcYaNUHGHjxHyB47R8geO0fIHjtHyB47RscdO0bIHTtGyB06RsgdOkbIHTpGyR06RcodOkXK' +
+	'HTlFyxw5RcscOUXLHDlEyxw5RMscOUTLHDhEyhw4RMocOETJHDhDyBs4Q8gbN0PHGzdDxxs3Q8YbN0PGGzdCxhs2QsYbNkLGGzZCxho2QsYaNkLGHjxHyB48' +
+	'R8gePEfHHjtHxx47R8ceO0bHHTtGyB07RsgdOkbIHTpGyR06RsodOkXKHTpFyx06RcsdOUXLHDlFyxw5RcscOUTKHDlEyhw4RMkcOETJHDhEyBw4RMgcOEPHG' +
+	'zdDxxs3Q8cbN0PHGzdDxxs3Q8cbN0LHGzZCxxs2QscaNkLHHjxIyB48R8cePEfHHjxHxx47R8ceO0fHHjtHyB47RsgdO0bJHTtGyR06RsodOkbKHTpGyx06' +
+	'RcsdOkXLHTlFyx05RcscOUXKHDlFyhw5RMkcOUTIHDhEyBw4RMccOETHHDhDxxs4Q8cbN0PHGzdDxxs3Q8cbN0PHGzdCxxs2QscbNkLHHz1IyB48SMcePEjH' +
+	'HjxHxx48R8cePEfHHjtHyB47R8geO0bJHTtGyR07RsodOkbKHTpGyh06RssdOkXLHTpFyx06RcodOUXKHDlFyRw5RckcOUTIHDlEyBw4RMccOETHHDhExxw4' +
+	'RMccOEPHGzhDxxs3Q8cbN0PIGzdDyBs3Q8gbN0LIHz1IyB89SMcePEjHHjxIxx48R8cePEfIHjxHyB47R8geO0bJHTtGyR07RsodOkbKHTpGyh06RssdOkXL' +
+	'HTpFyx06RcodOUXKHDlFyRw5RckcOUTIHDlEyBw4RMccOETHHDhExxw4RMccOEPHGzhDxxs3Q8cbN0PIGzdDyBs3Q8gbN0LIHz1IyB89SMgfPUjIHz1IyB48' +
+	'SMgePEjIHjxHyB48R8gePEfJHjtHyR47R8oeO0bKHTtGyh07RsodOkbKHTpGyh06RskdOkXJHTpFyB06RcgdOUXHHDlFxxw5RcccOUTHHDlExxw4RMccOETII' +
+	'DhEyBw4RMgcOEPIHz5JyR89ScgfPUnIHz1IyB89SMgfPUjJHz1IyR48SMkePEjJHjxHyh48R8oePEfKHjtHyh47R8oeO0bJHjtGyR07RsgdOkbIHTpGxx06' +
+	'RscdOkXGHTpFxh06RcYdOUXGHDlFxxw5RcccOUTHHDlEyBw4RMgcOETIHDhDyRs4Q8kbN0PIHz5JyR8+SckfPknJHz1JyR89SMkfPUjJHz1IyR89SMoePEjK' +
+	'HjxIyh48R8oePEfKHjxHyh48R8oeO0fJHjtHyR47RsgdO0bHHTtGxx06RsYdOkbGHTpGxh06RcYdOkXGHTlFxh05RcccOUXHHDlFyBw5RMgcOUTIHDhEyRw4' +
+	'RMkcOEPIHz5JyR8+SckfPknJHz5JyR89ScofPUnKHz1Iyh89SMofPUjKHz1Iyh48SMoePEjKHjxHyh48R8oePEfJHjtHyR47R8geO0fHHjtGxx07RsYdOkbG' +
+	'HTpGxh06RsYdOkXGHTpFxh06RccdOUXHHDlFyBw5RcgcOUTIHDlEyBw4RMgcOETIID5JyR8+SckfPknJHz5JyR89ScofPUnKHz1Iyh89SMofPUjKHz1Iyh48' +
+	'SMoePEjKHjxHyh48R8oePEfJHjtHyR47R8geO0fHHjtGxx07RsYdOkbGHTpGxh06RsYdOkXGHTpFxh06RccdOUXHHDlFyBw5RcgcOUTIHDlEyBw4RMgcOETII' +
+	'D5KyiA+SsogPknKHz5Jyh8+ScofPknKHz1Jyh89SMofPUjLHz1Iyx89SMsfPEjKHjxIyh48R8oePEfJHjxHyB48R8geO0fHHjtHxh47RsYdO0bGHTtGxh06' +
+	'RsYdOkbGHTpGxx06RccdOkXHHTlFyB05RcgcOUXIHDlFyBw5RMgcOUTIID9KyyA/SsogPkrKID5Jyx8+ScsfPknLHz5Jyx89ScsfPUnLHz1Iyx89SMsfPUjK' +
+	'Hz1Iyh48SMoePEjJHjxHyB48R8geO0fHHjtHxh47R8YeO0fGHjtGxh07RsYdO0bGHTpGxx06RscdOkXIHTpFyB06RcgdOUXIHDlFyBw5RcgcOUTIID9KyyA/' +
+	'SssgP0rLID5KyyA+SssgPknLHz5Jyx8+ScsfPknLHz1Jyx89SMsfPUjLHz1Iyh89SMofPEjJHjxIyB48R8gePEfHHjxHxx48R8YeO0fGHjtHxh47RsYdO0bH' +
+	'HTtGxx06RscdOkbIHTpGyB06RcgdOkXIHTlFyB05RcgcOUXHID9KyyA/SssgP0rLID9KyyA/SssgPkrLID5JyyA+ScwfPknLHz5Jyx89ScsfPUnLHz1Iyh89' +
+	'SMofPUjJHz1IyB48SMgePEjHHjxHxx48R8YePEfGHjtHxh47R8ceO0fHHjtGxx07RsgdO0bIHTpGyB06RsgdOkXIHTpFyB06RccdOUXHIEBLyyA/S8sgP0rL' +
+	'ID9KyyA/SswgP0rMID5KzCA+SswgPknMHz5Jyx8+ScsfPknLHz1Jyh89SMofPUjJHz1IyR89SMgfPEjHHjxIxx48R8cePEfHHjxHxx48R8ceO0fHHjtHyB47' +
+	'RsgdO0bIHTtGyB06RsgdOkbIHTpGyB06RccdOkXH';
+
+function decodeBase64Bytes(encoded) {
+	const binary = atob(encoded);
+	const out = new Uint8Array(binary.length);
+	for (let i = 0; i < binary.length; i += 1) out[i] = binary.charCodeAt(i);
+	return out;
+}
+
+let decoded = null;
+function decodedBake() {
+	if (decoded) return decoded;
+	const heightBytes = decodeBase64Bytes(G07_TERRAIN3D_PACKED_HEIGHTS_U16_LE_BASE64);
+	const rock = decodeBase64Bytes(G07_TERRAIN3D_PACKED_ROCK_U8_BASE64);
+	const colorRoughness = decodeBase64Bytes(G07_TERRAIN3D_PACKED_COLOR_ROUGHNESS_U8_BASE64);
+	const sampleCount = G07_TERRAIN3D_RUNTIME_BAKE.gridSize ** 2;
+	if (heightBytes.length !== sampleCount * 2 || rock.length !== sampleCount || colorRoughness.length !== sampleCount * 4) {
+		throw new Error('G07 Terrain3D packed runtime payload has invalid dimensions');
+	}
+	const heights = new Float64Array(sampleCount);
+	for (let i = 0; i < sampleCount; i += 1) {
+		const quantized = heightBytes[i * 2] | (heightBytes[i * 2 + 1] << 8);
+		heights[i] = G07_TERRAIN3D_RUNTIME_BAKE.heightOffsetMeters
+			+ quantized * G07_TERRAIN3D_RUNTIME_BAKE.heightQuantumMeters;
+	}
+	decoded = Object.freeze({ heights, rock, colorRoughness });
+	return decoded;
+}
+
+function bilinearIndex(normalizedX, normalizedY) {
+	if (!Number.isFinite(normalizedX) || !Number.isFinite(normalizedY)) throw new TypeError('normalized coordinates must be finite');
+	const bounds = G07_TERRAIN3D_RUNTIME_BAKE.normalizedBounds;
+	if (normalizedX < bounds.xMin || normalizedX > bounds.xMax || normalizedY < bounds.yMin || normalizedY > bounds.yMax) return null;
+	const size = G07_TERRAIN3D_RUNTIME_BAKE.gridSize;
+	const gx = ((normalizedX - bounds.xMin) / (bounds.xMax - bounds.xMin)) * (size - 1);
+	const gy = ((normalizedY - bounds.yMin) / (bounds.yMax - bounds.yMin)) * (size - 1);
+	const x0 = Math.floor(gx);
+	const y0 = Math.floor(gy);
+	const x1 = Math.min(x0 + 1, size - 1);
+	const y1 = Math.min(y0 + 1, size - 1);
+	return { x0, y0, x1, y1, tx: gx - x0, ty: gy - y0, size };
+}
+
+function bilerp(a, b, c, d, tx, ty) {
+	const top = a + (b - a) * tx;
+	const bottom = c + (d - c) * tx;
+	return top + (bottom - top) * ty;
+}
+
+export function sampleG07Terrain3dBakeNormalized(normalizedX, normalizedY) {
+	const q = bilinearIndex(normalizedX, normalizedY);
+	if (!q) return null;
+	const data = decodedBake();
+	const i00 = q.y0 * q.size + q.x0;
+	const i10 = q.y0 * q.size + q.x1;
+	const i01 = q.y1 * q.size + q.x0;
+	const i11 = q.y1 * q.size + q.x1;
+	const height = bilerp(data.heights[i00], data.heights[i10], data.heights[i01], data.heights[i11], q.tx, q.ty);
+	const rockBlend = bilerp(data.rock[i00], data.rock[i10], data.rock[i01], data.rock[i11], q.tx, q.ty) / 255;
+	const color = [];
+	for (let channel = 0; channel < 3; channel += 1) {
+		color.push(bilerp(
+			data.colorRoughness[i00 * 4 + channel],
+			data.colorRoughness[i10 * 4 + channel],
+			data.colorRoughness[i01 * 4 + channel],
+			data.colorRoughness[i11 * 4 + channel],
+			q.tx, q.ty,
+		) / 255);
+	}
+	const roughness = bilerp(
+		data.colorRoughness[i00 * 4 + 3],
+		data.colorRoughness[i10 * 4 + 3],
+		data.colorRoughness[i01 * 4 + 3],
+		data.colorRoughness[i11 * 4 + 3],
+		q.tx, q.ty,
+	) / 255;
+	return Object.freeze({
+		height,
+		rockBlend,
+		color: Object.freeze(color),
+		roughness,
+		snowBlend: 0,
+		roadBlend: 0,
+		pathBlend: 0,
+	});
+}
+
+export function getG07Terrain3dPackedPayloadForVerification() {
+	return Object.freeze({
+		heights: G07_TERRAIN3D_PACKED_HEIGHTS_U16_LE_BASE64,
+		rock: G07_TERRAIN3D_PACKED_ROCK_U8_BASE64,
+		colorRoughness: G07_TERRAIN3D_PACKED_COLOR_ROUGHNESS_U8_BASE64,
+	});
+}
