7ac3bc12af9b065fee580153532490b0c750a124
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-09-05 03:13:57 +0300

    test(ci): decouple creature threat smoke from full scene boot

--- scripts/smokeTestGame3D.js
+++ scripts/smokeTestGame3D.js
@@ -101,7 +101,7 @@ async function main() {
 		results.push(await movementChecks.checkWolfPatrol(committedBrowser, baseUrl));
 		results.push(await movementChecks.checkNpcCombatStance(committedBrowser, baseUrl));
 		results.push(await npcPerceptionChecks.checkNpcGuardPerception(committedBrowser, baseUrl));
-		results.push(await creatureThreatChecks.checkCreatureThreatMemory(browser, baseUrl));
+		results.push(await creatureThreatChecks.checkCreatureThreatMemory(committedBrowser, baseUrl));
 		results.push(await movementChecks.checkNpcAnimalCreatureObstacleCollider(committedBrowser, baseUrl));
 		results.push(await dragonFlightChecks.checkDragonFlight(browser, baseUrl));
 		results.push(await dragonFlightChecks.checkDragonNotice(browser, baseUrl));
