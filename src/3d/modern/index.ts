4fe37d47af1ba7300b75a7b6ce782d3954bc4230
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-09-16 11:01:00 +0300

    feat(modern): expose typed Game3D entry composition root

--- src/3d/modern/index.ts
+++ src/3d/modern/index.ts
@@ -46,3 +46,5 @@ export * from './networkSnapshot';
 export * from './networkReplication';
 export * from './runtimeConfig';
 export * from './serialization';
+export * from './entryGate';
+export * from './gameEntry';
