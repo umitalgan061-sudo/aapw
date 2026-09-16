30b19ac0c198e85c716a4c04c4526f1c48611cc8
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-09-16 15:33:37 +0300

    feat(runtime-v3): expose native TypeScript runtime surface

---
+++ src/3d/modern/v3/index.ts
@@ -0,0 +1,5 @@
+export * from './runtimeContracts.js';
+export * from './runtimeKernel.js';
+export * from './legacyGameAdapter.js';
+export * from './browserFrameSource.js';
+export * from './runtimeApplication.js';
