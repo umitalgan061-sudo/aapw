f17087c9d452f254c54c77beaa9d2bda503d2b26
umitalgan061-sudo <umitalgan.061@gmail.com>
2026-09-21 09:38:43 +0300

    refactor(world): connect canonical rock shadow to typed terrain owner

--- src/3d/world/worldReferenceRockShadow.js
+++ src/3d/world/worldReferenceRockShadow.js
@@ -9,7 +9,7 @@
 
 import * as THREE from 'three';
 import { WORLD_DEFAULTS, SETTLEMENT_CONFIG } from '../config.js';
-import { createHeightSampler } from './terrain.js';
+import { createHeightSampler } from './terrain.ts';
 import { KINGDOM_SEATS, computeSettlementFlattenPads } from './settlements.js';
 import { computeSeatMST } from './roads.js';
 import { findSlopeAwarePath } from './roadPathfinder.js';
